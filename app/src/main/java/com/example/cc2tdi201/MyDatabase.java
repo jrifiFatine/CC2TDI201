a9ec9d67b4e5a6613349c0dfdd2677501a186b69
hp <hp@DESKTOP-PDLTMSV>
2022-06-03 15:36:56 +0100

    Q5

--- app/src/main/java/com/example/cc2tdi201/MyDatabase.java
+++ app/src/main/java/com/example/cc2tdi201/MyDatabase.java
@@ -1,10 +1,14 @@
 package com.example.cc2tdi201;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
 
 public class MyDatabase extends SQLiteOpenHelper {
     public static String DB_NAME="Societe.db";
@@ -26,6 +30,61 @@ public class MyDatabase extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        String sql= "DROP TABLE " + TABLE_NAME;
+        sqLiteDatabase.execSQL(sql);
+        onCreate(sqLiteDatabase);
 
     }
+    public static long ADDSociete(SQLiteDatabase sqLiteDatabase,Societe e){
+        ContentValues cv = new ContentValues();
+        cv.put(COL2,e.getNom());
+        cv.put(COL3,e.getSecteur_Activite());
+        cv.put(COL4,e.getNombre_employe());
+
+        return sqLiteDatabase.insert(TABLE_NAME,null,cv);
+    }
+    public static long UpdateSociete(SQLiteDatabase sqLiteDatabase, Societe e){
+        ContentValues cv = new ContentValues();
+        cv.put(COL2,e.getNom());
+        cv.put(COL3,e.getSecteur_Activite());
+        cv.put(COL4,e.getNombre_employe());
+        return sqLiteDatabase.update(TABLE_NAME,cv,"id="+e.getId(),null);
+    }
+    public static long deleteSociete(SQLiteDatabase sqLiteDatabase, int id){
+        return sqLiteDatabase.delete(TABLE_NAME,"id="+id,null);
+    }
+    public static ArrayList<Societe> getAllSociete(SQLiteDatabase sqLiteDatabase){
+        ArrayList<Societe> soc = new ArrayList<>();
+
+        Cursor cr = sqLiteDatabase.rawQuery("SELECT * FROM " + TABLE_NAME,null);
+
+        while(cr.moveToNext()){
+            Societe e = new Societe();
+            e.setId(cr.getInt(0));
+            e.setNom(cr.getString(1));
+            e.setSecteur_Activite(cr.getString(2));
+            e.setNombre_employe(cr.getInt(3));
+
+            soc.add(e);
+        }
+
+        return soc;
+    }
+    public static Societe getOneSociete(SQLiteDatabase sqLiteDatabase, int id){
+        Societe e = null;
+
+        Cursor cr = sqLiteDatabase.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE id = " + id,null);
+
+        if(cr.moveToNext()){
+            e = new Societe();
+            e.setId(cr.getInt(0));
+            e.setNom(cr.getString(1));
+            e.setSecteur_Activite(cr.getString(2));
+            e.setNombre_employe(cr.getInt(3));
+
+        }
+
+        return e;
+    }
+
 }
