d3fc9840056cd1ad7c409680738e58155f7813a3
hp <hp@DESKTOP-PDLTMSV>
2022-06-03 16:07:03 +0100

    Q7

--- app/src/main/java/com/example/cc2tdi201/MainActivity.java
+++ app/src/main/java/com/example/cc2tdi201/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.cc2tdi201;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void acess(View view) {
+
+        Intent i = null;
+        switch (view.getId()){
+            case R.id.b1: i=new Intent(MainActivity.this, AjouterSociete.class); break;
+            case R.id.b2: i=new Intent(MainActivity.this, EditerSociete.class); break;
+            case R.id.b3: i=new Intent(MainActivity.this, ListeTousSociete.class); break;
+
+        }
+        startActivity(i);
     }
 }
